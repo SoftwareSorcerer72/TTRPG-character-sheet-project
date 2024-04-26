3017f94c191c5e966c47dd1c369f6989eaef356a
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-04-26 06:31:29 -0400

    Deleted duplicate json files from install done in parent folder

--- capstone-front-end/my-react-app/src/components/NavBar.tsx
+++ capstone-front-end/my-react-app/src/components/NavBar.tsx
@@ -14,19 +14,19 @@ const NavBar: React.FC = () => {
   return (
     <AppBar position="static" sx={{ backgroundColor: 'red' }}>
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1, marginLeft: '10px' }}>
           TTRPG Grimoire
         </Typography>
         {isLoggedIn ? (
           <>
-            <Button color="inherit" component={Link} to="/">Dashboard</Button>
-            <Button color="inherit" component={Link} to="/edit-user">Edit User</Button>
-            <Button color="inherit" onClick={handleSignOut}>Sign Out</Button>
+            <Button color="inherit" component={Link} to="/" sx={{ marginRight: '20px', '&:hover': { boxShadow: '0 0 10px purple' } }}>Dashboard</Button>
+            <Button color="inherit" component={Link} to="/edit-user" sx={{ marginRight: '20px', '&:hover': { boxShadow: '0 0 10px purple' } }}>Edit User</Button>
+            <Button color="inherit" onClick={handleSignOut} sx={{ marginRight: '20px', '&:hover': { boxShadow: '0 0 10px purple' } }}>Sign Out</Button>
           </>
         ) : (
           <>
-            <Button color="inherit" component={Link} to="/login">Log In</Button>
-            <Button color="inherit" component={Link} to="/signup">Sign Up</Button>
+            <Button color="inherit" component={Link} to="/login" sx={{ marginRight: '20px', '&:hover': { boxShadow: '0 0 10px purple' } }}>Log In</Button>
+            <Button color="inherit" component={Link} to="/signup" sx={{ marginRight: '20px', '&:hover': { boxShadow: '0 0 10px purple' } }}>Sign Up</Button>
           </>
         )}
       </Toolbar>
