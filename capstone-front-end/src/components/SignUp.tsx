3f589d882422dae1bcfd8a382bd2fa7668f3fa2a
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-04-26 11:20:46 -0400

    Fixed edit character sheet issue on flask and front end.

--- capstone-front-end/src/components/SignUp.tsx
+++ capstone-front-end/src/components/SignUp.tsx
@@ -3,33 +3,36 @@ import { TextField, Button, Typography, Container } from '@mui/material';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const Login: React.FC = () => {
+
+const SignUp: React.FC = () => {
+  const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-  const handleLogin = async (event: React.FormEvent) => {
+  const handleSignUp = async (event: React.FormEvent) => {
     event.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/login', { email, password });
-      localStorage.setItem('token', response.data.access_token);
+      await axios.post('https://ttrpg-backend.onrender.com/signup', { email, password, username });
+      alert('Sign up successful');
       navigate('/');
     } catch (error) {
-      alert('Login failed');
+      alert('Sign up failed');
       console.error(error);
     }
   };
 
   return (
     <Container sx={{ backgroundColor: 'rgba(173, 216, 230, 0.8)', padding: '20px', borderRadius: '10px', marginTop: '20px' }}>
-      <form onSubmit={handleLogin}>
-        <Typography variant="h6" sx={{ backgroundColor: 'rgba(173, 216, 230, 0.8)', marginBottom: '10px' }}>Login</Typography>
+      <form onSubmit={handleSignUp}>
+        <Typography variant="h6" sx={{ backgroundColor: 'rgba(173, 216, 230, 0.8)', marginBottom: '10px' }}>Sign Up</Typography>
+        <TextField label="Username" fullWidth required value={username} onChange={e => setUsername(e.target.value)} sx={{ backgroundColor: 'rgba(173, 216, 230, 0.8)', marginBottom: '10px' }} />
         <TextField label="Email" type="email" fullWidth required value={email} onChange={e => setEmail(e.target.value)} sx={{ backgroundColor: 'rgba(173, 216, 230, 0.8)', marginBottom: '10px' }} />
         <TextField label="Password" type="password" fullWidth required value={password} onChange={e => setPassword(e.target.value)} sx={{ backgroundColor: 'rgba(173, 216, 230, 0.8)', marginBottom: '10px' }} />
-        <Button type="submit" color="primary" variant="contained" sx={{ backgroundColor: 'purple', '&:hover': { backgroundColor: 'rgb(70, 130, 180)' } }}>Login</Button>
+        <Button type="submit" color="primary" variant="contained" sx={{ backgroundColor: 'purple', '&:hover': { backgroundColor: 'rgb(70, 130, 180)' } }}>Sign Up</Button>
       </form>
     </Container>
   );
 };
 
-export default Login;
+export default SignUp;
